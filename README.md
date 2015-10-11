fdc17f505654a63dbbc7b25994c5ae820acf37f1
gunjankhandelwal <gunjan.khandelwal@sjsu.edu>
2015-10-11 16:47:22 -0700

    1st change

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-[![Stories in Ready](https://badge.waffle.io/Glucosio/android.png?label=ready&title=Ready)](https://waffle.io/Glucosio/android)
-[![Crowdin](https://d322cqt584bo4o.cloudfront.net/glucosio/localized.png)](https://crowdin.com/project/glucosio)
-[![Build Status](https://travis-ci.org/Glucosio/android.svg)](https://travis-ci.org/Glucosio/android)
+
 # CMPE295B project- Connected Healthcare using wearable technology for Android
 
 
--- app/src/main/java/org/glucosio/android/activity/LoginActivity.java
+++ app/src/main/java/org/glucosio/android/activity/LoginActivity.java
@@ -163,7 +163,8 @@ public class LoginActivity extends AppCompatActivity {
         presenter.saveToDatabase();
     }
     public void closeHelloActivity(){
-        Intent intent = new Intent(this, MainActivity.class);
+
+        Intent intent = new Intent(this, BluetoothActivity.class);
         startActivity(intent);
         finish();
     }
@@ -203,116 +204,3 @@ public class LoginActivity extends AppCompatActivity {
 
 }
 
-
-/**************
-
- presenter = new LoginPresenter(this);
- presenter.loadDatabase();
-
- firstView = (ScrollView) findViewById(R.id.helloactivity_mainframe);
- EULAView = (ScrollView) findViewById(R.id.helloactivity_eulaframe);
- EULACheckbox = (CheckBox) findViewById(R.id.helloactivity_checkbox_eula);
- languageSpinner = (LabelledSpinner) findViewById(R.id.helloactivity_spinner_language);
- genderSpinner = (LabelledSpinner) findViewById(R.id.helloactivity_spinner_gender);
- typeSpinner = (LabelledSpinner) findViewById(R.id.helloactivity_spinner_diabetes_type);
- unitSpinner = (LabelledSpinner) findViewById(R.id.helloactivity_spinner_preferred_unit);
- startButton = (Button) findViewById(R.id.helloactivity_start);
-
- termsTextView = (TextView) findViewById(R.id.helloactivity_textview_terms);
-
- ageTextView = (TextView) findViewById(R.id.helloactivity_age);
- nextButton = (Button) findViewById(R.id.helloactivity_next);
-
- // Populate Spinners with array
- languageSpinner.setItemsArray(R.array.helloactivity_language_list);
- genderSpinner.setItemsArray(R.array.helloactivity_gender_list);
- unitSpinner.setItemsArray(R.array.helloactivity_preferred_unit);
- typeSpinner.setItemsArray(R.array.helloactivity_diabetes_type);
-
- termsTextView.setMovementMethod(new ScrollingMovementMethod());
- EULACheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-@Override
-public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-if (isChecked) {
-startButton.setEnabled(true);
-} else {
-startButton.setEnabled(false);
-}
-}
-}
- );
-
- //TODO: add Preferred Unit and Diabetes Type in dB
- }
-
- public void onNextClicked(View v){
- presenter.onNextClicked(ageTextView.getText().toString(),
- genderSpinner.getSpinner().getSelectedItemPosition(), languageSpinner.getSpinner().getSelectedItem().toString());
- }
-
- public void showEULA(){
- // Prepare the View for the animation
- firstView.animate()
- .alpha(0f)
- .setListener(new AnimatorListenerAdapter() {
-@Override
-public void onAnimationEnd(Animator animation) {
-super.onAnimationEnd(animation);
-firstView.setVisibility(View.GONE);
-showEULAAnimation();
-}
-});
- }
-
- private void showEULAAnimation() {
- // Prepare the View for the animation
- EULAView.setVisibility(View.VISIBLE);
- EULAView.setAlpha(0.0f);
-
- EULAView.animate()
- .alpha(1f);
- firstView.setVisibility(View.GONE);
- }
-
-
- public void onStartClicked(View v){
- presenter.saveToDatabase();
- }
-
- public void displayErrorMessage(){
- Toast.makeText(getApplicationContext(), getString(R.string.helloactivity_age_invalid), Toast.LENGTH_SHORT).show();
- }
-
- public void closeHelloActivity(){
- Intent intent = new Intent(this, MainActivity.class);
- startActivity(intent);
- finish();
- }
-
- public void showToast(String text){
- Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
- }
-
- @Override
- public boolean onCreateOptionsMenu(Menu menu) {
- // Inflate the menu; this adds items to the action bar if it is present.
- getMenuInflater().inflate(R.menu.menu_hello, menu);
- return true;
- }
-
- @Override
- public boolean onOptionsItemSelected(MenuItem item) {
- // Handle action bar item clicks here. The action bar will
- // automatically handle clicks on the Home/Up button, so long
- // as you specify a parent activity in AndroidManifest.xml.
- int id = item.getItemId();
-
- //noinspection SimplifiableIfStatement
- if (id == R.id.action_settings) {
- return true;
- }
-
- return super.onOptionsItemSelected(item);
- }
- }
- */
